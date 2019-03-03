d596047ac488f6bddc14963d7c8bf3ff4b7a9213
ajlopez <webmaster@ajlopez.com>
2019-03-03 10:05:12 -0300

    Max bet and fee

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.4.24;
 contract Bet {
     uint public pot;
     uint public accum;
+    uint public maxbet;
+    uint8 public fee;
+
+    constructor(uint _maxbet, uint8 _fee) public {
+        require(_fee < 100);
+        maxbet = _maxbet;
+        fee = _fee;
+    }
     
     function bet() public payable {
+        require(msg.value > 0);
+        require(msg.value <= maxbet);
+        
         uint divisor = 3 + pot / msg.value;
         
         pot += msg.value;
@@ -12,13 +23,15 @@ contract Bet {
         accum += uint(msg.sender);
         accum += block.number;
         accum += now;
+        accum += msg.value;
         
         uint result = accum % divisor;
         
         if (result == 0) {
-            uint price = pot;
+            uint prize = pot * (100 - fee)/ 100;
             pot = 0;
-            msg.sender.transfer(price);
+            msg.sender.transfer(prize);
         }
     }
 }
+
--- test/Bet_test.js
+++ test/Bet_test.js
@@ -1,9 +1,11 @@
 
 const Bet = artifacts.require('./Bet.sol');
 
+const expectThrow = require('./utils').expectThrow;
+
 contract('Bet', function (accounts) {
     beforeEach(async function () {
-        this.bet = await Bet.new();
+        this.bet = await Bet.new(2000, 10);
     });
     
     it('zero pot', async function () {
@@ -18,5 +20,18 @@ contract('Bet', function (accounts) {
         const pot = (await this.bet.pot()).toNumber();
         
         assert.ok(pot === 1000 || pot === 0);
+        
+        if (pot === 0) {
+            const balance = await web3.eth.getBalance(this.bet.address);
+            assert.equal(balance, 100);
+        }
+    });
+
+    it('cannot bet 0 value', async function () {
+        expectThrow(this.bet.bet({ value: 0 }));
+    });
+    
+    it('cannot bet more than max value', async function () {
+        expectThrow(this.bet.bet({ value: 3000 }));
     });
 });
